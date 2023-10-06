9114aebb6ee3404918f3c7b73d6e9b21a94c52d1
kohei <amayiehok213@gmail.com>
2023-10-06 20:00:30 +0900

    ハンバーガーメニュー作成

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,13 @@
-$(window).scroll(function (){
+$(window).scroll(function() {
     if (window.scrollY >= 51) {
         $("header").addClass("scrolled");
     } else {
         $("header").removeClass("scrolled");
     }
 });
+
+$("#js-hamburger").click(function() {
+    $(this).toggleClass("clicked");
+    $(".hnav-container").toggleClass("opened");
+    $(".black-screen").toggleClass("displayed");
+})
